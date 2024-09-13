66bcf191105c0477f1b9c88e23cf02499e096b9f
Ishaan Bhatia <ishaanratanbhatia@Ishaans-MacBook-Pro.local>
2024-09-13 01:20:26 +0100

    Na, taking back the home page as it was

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,16 +1,45 @@
+// import Image from 'next/image';
+// import heroImage from '../../public/hero.jpg';
+
+// const Hero = () => {
+//   return (
+//     <div className="flex justify-center items-center h-screen mt-10"> {/* mt-10 adds margin to the top */}
+//       <div className="w-full max-w-2xl">
+//         <Image
+//           src={heroImage}
+//           alt="Hero Image"
+//           className="w-full h-auto"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hero;
+
 import Image from 'next/image';
 import heroImage from '../../public/hero.jpg';
 
 const Hero = () => {
+  const aspectRatio = 3 / 2;
+  const maxWidthPercentage = 80;
+  const screenWidth = typeof window !== 'undefined' ? window.innerWidth : 1920;
+  const maxWidth = (maxWidthPercentage / 100) * screenWidth;
+  const height = maxWidth / aspectRatio;
+
   return (
-    <div className="flex justify-center items-center h-screen mt-10"> {/* mt-10 adds margin to the top */}
-      <div className="w-full max-w-2xl">
-        <Image
-          src={heroImage}
-          alt="Hero Image"
-          className="w-full h-auto"
-        />
-      </div>
+    <div className="max-w-screen-xl mx-auto">
+      <section className="h-auto mx-auto lg:py-8 pb-2 flex">
+        <div className="w-full lg:w-2/3 mx-auto">
+          <Image
+            src={heroImage}
+            alt="Hero Image"
+            className="w-full h-auto object-cover mx-auto my-auto px-4 lg:px-0"
+            width={maxWidth}
+            height={height}
+          />
+        </div>
+      </section>
     </div>
   );
 };
