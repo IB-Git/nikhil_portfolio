35d69de095e695f43d0640bf458f8e4fe5fb6b4e
Ishaan Bhatia <ishaanratanbhatia@Ishaans-MacBook-Pro.local>
2024-09-13 21:15:13 +0100

    trynna make it  work on iphone - 2nd try

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self';" />
-      </Head>
       <body>
         <Main />
         <NextScript />
