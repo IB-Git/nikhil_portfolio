4a7df5be31bd0d909ea3ba52dce0146de624661d
Ishaan Bhatia <ishaanratanbhatia@Ishaans-MacBook-Pro.local>
2024-09-13 21:21:13 +0100

    trynna make it  work on iphone - 4th try

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <body>
-      <Head>
-      <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self';" />
-      </Head>
         <Main />
         <NextScript />
       </body>
