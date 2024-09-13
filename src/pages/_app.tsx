487a10a13d7dd552dc5e241a28a1894dcb7e1302
Ishaan Bhatia <ishaanratanbhatia@Ishaans-MacBook-Pro.local>
2024-09-13 21:17:29 +0100

    trynna make it  work on iphone - 3rd try

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
     <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self';" />
     </Head>
     <Component {...pageProps} />
     </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <body>
+      <Head>
+      <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self';" />
+      </Head>
         <Main />
         <NextScript />
       </body>
