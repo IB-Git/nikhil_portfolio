51bd3d4d7d13472d05f275af7ad4af0049e03a8a
Ishaan Bhatia <ishaanratanbhatia@Ishaans-MacBook-Pro.local>
2024-09-15 20:00:50 +0100

    fixed default page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,24 +5,30 @@ import photoshootsData from '../../public/photoshoots.json';
 const Header = () => {
   const router = useRouter();
   const { id } = router.query;
+  
+  const defaultCollectionId = photoshootsData[0]?.id;
 
   return (
     <div className="pt-4 pl-4 mb-2 lg:pt-8 lg:pl-8 lg:absolute left-0 top-0 lg:h-full lg:text-right">
       <header>
         <nav>
           <ul className="space-y-1">
+            {/* About Link */}
             <li>
-              <Link href="/about" className="text-xl font-bold-about">
+              <Link href="/about" className="text-xl font-nohemi-regular font-bold-about">
                 About
               </Link>
             </li>
 
+            {/* Collection Names */}
             {photoshootsData.map((photoshoot) => (
               <li key={photoshoot.id}>
                 <Link
                   href={`/photography/${photoshoot.id}`}
                   className={`text-lg ${
-                    id === photoshoot.id ? 'font-nohemi-medium' : 'font-nohemi-light'
+                    id === photoshoot.id || (!id && photoshoot.id === defaultCollectionId)
+                      ? 'font-nohemi-medium'
+                      : 'font-nohemi-light'
                   }`}
                 >
                   {photoshoot.title}
