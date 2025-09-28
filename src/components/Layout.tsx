d34a0c355fb025dd0d959b9353d29a2f39cdc775
Ishaan Bhatia <ishaanratanbhatia@Ishaans-MacBook-Pro.local>
2025-09-28 01:45:58 +0100

    Fixing import error 1

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import Header from './Header'; 
 import Head from 'next/head';
-import Header from '@/components/Header';
 
 interface LayoutProps {
   children: React.ReactNode;
