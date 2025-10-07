93f90451a6d07efe8699039b13e521c1c5472bd8
Ishaan Bhatia <ishaanratanbhatia@Ishaans-MacBook-Pro.local>
2025-10-07 13:30:47 +0100

    added space-4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ const Home = () => {
   }
 
   return (
-    <div className="space-y-8 lg:space-y-16 p-4"> 
+    <div className="space-y-8 lg:space-y-32 p-4"> 
       {photoshoots.map((shoot) => {
         const imageCount = shoot.images.length;
         const gridClass = getGridClass(imageCount);
