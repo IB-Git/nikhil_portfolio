0abec51e92e93a0b985849d8bcc4beacc54ea0d7
Ishaan Ratan Bhatia <69042024+IB-Git@users.noreply.github.com>
2025-10-13 02:57:13 +0100

    Updated film.tsx

--- src/pages/film.tsx
+++ src/pages/film.tsx
@@ -57,7 +57,7 @@ const FilmPage = () => {
   }
 
   return (
-    <div className="space-y-8 lg:space-y-32 p-4"> 
+    <div className="space-y-8 lg:space-y-48 p-4"> 
       {photoshoots.map((shoot) => {
         const imageCount = shoot.images.length;
         const gridClass = getGridClass(imageCount);
